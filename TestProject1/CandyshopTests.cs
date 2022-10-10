c4614efae814140b38ed689884461a9df763d7eb
Jonathan Johansson <jonathan.johansson@edu.edugrade.se>
2022-10-10 13:57:28 +0200

    Finished Sales Tests

--- TestProject1/CandyshopTests.cs
+++ TestProject1/CandyshopTests.cs
@@ -16,7 +16,7 @@ namespace Candyshop.Tests
         private readonly Mock<AppDbContext> _appDbContexMockRepo;
         private readonly Mock<ICandyRatingRepository> _CandyRatingRepoMockRepo;
         private readonly CandyController _candyController;
-        
+
 
         public Tests()
         {
@@ -26,19 +26,19 @@ namespace Candyshop.Tests
             _CandyRatingRepoMockRepo = new Mock<ICandyRatingRepository>();
             _candyController = new CandyController(_candyMockRepo.Object, _categoryMockRepo.Object, _appDbContexMockRepo.Object, _CandyRatingRepoMockRepo.Object);
 
-            
+
         }
 
 
         [SetUp]
         public void Setup()
         {
-            
+
 
         }
 
         [TestCase("Tasty")]
-        public void SearchFunction( string searchTerm)
+        public void SearchFunction(string searchTerm)
         {
 
             //arrange
@@ -86,9 +86,33 @@ namespace Candyshop.Tests
          });
             string category = "TestCategory";
             bool testBool = true;
+            bool testIsOnSaleBool = false;
 
             //Act
-            var result = _candyController.List(category, testBool) as ViewResult;
+            var result = _candyController.List(category, testBool, testIsOnSaleBool) as ViewResult;
+            var candies = result.Model as CandyListViewModel;
+
+            //Assert
+            Assert.That(candies.Candies.Count, Is.EqualTo(2));
+        }
+
+        [TestCase]
+        public void CheckForSales()
+        {
+
+            //arrange
+            _candyMockRepo.Setup(repo => repo.GetAllCandy)
+         .Returns(new List<Candy>() {
+             new Candy { CandyId = 1, AmountInStock = 101, CategoryId = 1, Description = "candy1", ImageThumbnailUrl = "canyThumb_1.png", ImageUrl = "canyPic_1.png", IsInStock = true, IsOnSale = false, IsNew = false, Name = "QuiteTastyCandy1", Price = 100000, SaleStartDate = new DateTime(2023,10,10), SaleEndDate = new DateTime(2025,10,10)},
+             new Candy { CandyId = 2, AmountInStock = 102, CategoryId = 2, Description = "candy2", ImageThumbnailUrl = "candyThumb_2.png", ImageUrl = "canyPic_2.png", IsInStock = true, IsOnSale = true, IsNew = false, Name = "QuiteTastyCandy2", Price = 200000, SaleStartDate = new DateTime(2022,10,10), SaleEndDate = new DateTime(2025,10,10)},
+             new Candy { CandyId = 3, AmountInStock = 5000, CategoryId = 3, Description = "candy3", ImageThumbnailUrl = "candyThumb_3.png", ImageUrl = "canyPic_3.png", IsInStock = true, IsOnSale = true, IsNew = false, Name = "QuiteTastyCandy3", Price = 300000, SaleStartDate = new DateTime(2022,10,10), SaleEndDate = new DateTime(2025,10,10)}
+         });
+            string category = "HOT SALES";
+            bool testIsNewBool = false;
+            bool testIsOnSale = true;
+
+            //Act
+            var result = _candyController.List(category, testIsNewBool, testIsOnSale) as ViewResult;
             var candies = result.Model as CandyListViewModel;
 
             //Assert
